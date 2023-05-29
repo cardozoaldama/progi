43e57306edcd4d15489a020c11af64e3f09ec99c
Fernando Cardozo <nuevo_10000@outlook.com>
2023-05-29 15:03:29 -0400

    New classes for improving the project

---
+++ prog_i/src/main/java/dao/ArticuloDAO.java
@@ -0,0 +1,124 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import conexion.Conexion;
+import entidad.Articulo;
+
+public class ArticuloDAO {
+	private Conexion con;
+	private Connection connection;
+
+	public ArticuloDAO(String URL, String USUARIO, String CLAVE) throws SQLException {
+		System.out.println(URL);
+		con = new Conexion();
+	}
+	
+	// insertar artículo
+	public boolean insertar(Articulo articulo) throws SQLException {
+		String sql = "INSERT INTO articulos (idarticulo, codigo, nombre, descripcion, existencia, precio) VALUES (?, ?, ?,?,?,?)";
+		System.out.println(articulo.getDescripcion());
+		con.conectar();
+		connection = con.getJdbcConnection();
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setString(1, null);
+		statement.setString(2, articulo.getCodigo());
+		statement.setString(3, articulo.getNombre());
+		statement.setString(4, articulo.getDescripcion());
+		statement.setDouble(5, articulo.getExistencia());
+		statement.setDouble(6, articulo.getPrecio());
+
+		boolean rowInserted = statement.executeUpdate() > 0;
+		statement.close();
+		con.desconectar();
+		return rowInserted;
+	}
+
+	// listar todos los productos
+	public List<Articulo> listarArticulos() throws SQLException {
+
+		List<Articulo> listaArticulos = new ArrayList<Articulo>();
+		String sql = "SELECT * FROM articulos";
+		con.conectar();
+		connection = con.getJdbcConnection();
+		Statement statement = connection.createStatement();
+		ResultSet resulSet = statement.executeQuery(sql);
+
+		while (resulSet.next()) {
+			int idarticulo = resulSet.getInt("idarticulo");
+			String codigo = resulSet.getString("codigo");
+			String nombre = resulSet.getString("nombre");
+			String descripcion = resulSet.getString("descripcion");
+			Double existencia = resulSet.getDouble("existencia");
+			Double precio = resulSet.getDouble("precio");
+			Articulo articulo = new Articulo(idarticulo, codigo, nombre, descripcion, existencia, precio);
+			listaArticulos.add(articulo);
+		}
+		con.desconectar();
+		return listaArticulos;
+	}
+
+	// obtener por id
+	public Articulo obtenerPorId(int idarticulo) throws SQLException {
+		Articulo articulo = null;
+
+		String sql = "SELECT * FROM articulos WHERE idarticulo= ? ";
+		con.conectar();
+		connection = con.getJdbcConnection();
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setInt(1, idarticulo);
+
+		ResultSet res = statement.executeQuery();
+		if (res.next()) {
+			articulo = new Articulo(res.getInt("idarticulo"), res.getString("codigo"), res.getString("nombre"),
+					res.getString("descripcion"), res.getDouble("existencia"), res.getDouble("precio"));
+		}
+		res.close();
+		con.desconectar();
+
+		return articulo;
+	}
+
+	// actualizar
+	public boolean actualizar(Articulo articulo) throws SQLException {
+		boolean rowActualizar = false;
+		String sql = "UPDATE articulos SET codigo=?,nombre=?,descripcion=?,existencia=?, precio=? WHERE idarticulo=?";
+		con.conectar();
+		connection = con.getJdbcConnection();
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setString(1, articulo.getCodigo());
+		statement.setString(2, articulo.getNombre());
+		statement.setString(3, articulo.getDescripcion());
+		statement.setDouble(4, articulo.getExistencia());
+		System.out.println(articulo.getPrecio());
+		statement.setDouble(5, articulo.getPrecio());
+		statement.setInt(6, articulo.getIdarticulo());
+
+		rowActualizar = statement.executeUpdate() > 0;
+		statement.close();
+		con.desconectar();
+		return rowActualizar;
+	}
+	
+	//eliminar
+	public boolean eliminar(Articulo articulo) throws SQLException {
+		boolean rowEliminar = false;
+		String sql = "DELETE FROM articulos WHERE idarticulo=?";
+		con.conectar();
+		connection = con.getJdbcConnection();
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setInt(1, articulo.getIdarticulo());
+
+		rowEliminar = statement.executeUpdate() > 0;
+		statement.close();
+		con.desconectar();
+
+		return rowEliminar;
+	}
+}
---
+++ prog_i/src/main/java/entidad/Articulo.java
@@ -0,0 +1,59 @@
+package entidad;
+
+public class Articulo {
+	private int idarticulo;
+	private String codigo;
+	private String nombre;
+	private String descripcion;
+	private double existencia;
+	private double precio;
+	
+	public Articulo(int idarticulo, String codigo, String nombre, String descripcion, double existencia, double precio) {
+		this.idarticulo = idarticulo;
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.existencia = existencia;
+		this.precio = precio;
+	}
+	//getters y setters
+
+	public String getCodigo() {
+		return codigo;
+	}
+	public int getIdarticulo() {
+		return idarticulo;
+	}
+
+	public void setIdarticulo(int idarticulo) {
+		this.idarticulo = idarticulo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public double getExistencia() {
+		return existencia;
+	}
+	public void setExistencia(double existencia) {
+		this.existencia = existencia;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+}
