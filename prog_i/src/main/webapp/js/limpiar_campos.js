3565fceebc80059600ad0da86e96b6f252371d9b
Fernando Cardozo <nuevo_10000@outlook.com>
2023-06-07 00:52:39 -0400

    New JS file for clear the form

---
+++ prog_i/src/main/webapp/js/limpiar_campos.js
@@ -0,0 +1,12 @@
+/**
+ * LIMPIAR DATOS DEL FORMULARIO
+ */
+
+function LimpiarCampos() {
+	document.getElementById("codigo").value = "";
+	document.getElementById("nombre").value = "";
+	document.getElementById("descripcion").value = "";
+	document.getElementById("cantidad").value = "";
+	document.getElementById("precio").value = "";
+	// El resto de los campos de texto
+}
